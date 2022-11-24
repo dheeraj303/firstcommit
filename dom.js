751b4a2df2aa45a24f52240d16c7eeb15fe22dbd
dheeraj303 <singh.dheeraj6303@gmail.com>
2022-11-24 21:22:47 +0530

    dom and test1

---
+++ dom.js
@@ -0,0 +1,24 @@
+// // console.log(document);
+// console.log(document.title);
+// document.title="hello";
+// console.log(document.all[7]);
+
+// var head=document.getElementById('header-title');
+// var head1=document.getElementById('main-header');
+// head1.style.borderBottom = "thick solid #0000FF";
+// head.style.color="red";
+
+var items=document.getElementsByClassName('list-group-item');
+console.log(items);
+console.log(items[1]);
+items[1].textContent='Hello 2';
+// items[1].style.fontWeight='bold';
+items[1].style.color='green';
+items[2].style.backgroundColor='green';
+
+for(var i=0;i<items.length;i++){
+    items[i].style.fontWeight='bold';
+}
+
+
+
