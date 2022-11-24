386f2583597740112eaed564ce9503c502bd3bf4
dheeraj303 <singh.dheeraj6303@gmail.com>
2022-11-24 22:06:58 +0530

    dom manipulation

--- dom.js
+++ dom.js
@@ -58,17 +58,72 @@
 // seconditem.style.color='coral';
 
 //Query selector all
-var title=document.querySelectorAll('.title');
-title[0].textContent='hello';
+// var title=document.querySelectorAll('.title');
+// title[0].textContent='hello';
 
-var odd=document.querySelectorAll('li:nth-child(odd)');
-var even=document.querySelectorAll('li:nth-child(even)');
+// var odd=document.querySelectorAll('li:nth-child(odd)');
+// var even=document.querySelectorAll('li:nth-child(even)');
 
-for(var i=0;i<odd.length;i++){
-    odd[i].style.backgroundColor='red';
-    even[i].style.backgroundColor='blue';
-}
-
-
+// for(var i=0;i<odd.length;i++){
+//     odd[i].style.backgroundColor='red';
+//     even[i].style.backgroundColor='blue';
+// }
 
 
+
+//Traversing the dom
+
+var itemlist=document.querySelector('#items');
+
+console.log(itemlist.parentNode);
+itemlist.parentNode.style.backgroundColor='red';
+console.log(itemlist.parentNode.parentNode.parentNode);
+
+
+console.log(itemlist.parentElement);
+itemlist.parentElement.style.backgroundColor='red';
+console.log(itemlist.parentElement.parentElement.parentElement);
+
+
+//Child node
+
+console.log(itemlist.childNodes);
+
+console.log(itemlist.children);;
+
+
+console.log(itemlist.children[1]);
+itemlist.children[1].style.backgroundColor='yellow';
+
+//first child
+
+console.log(itemlist.firstChild);
+console.log(itemlist.firstElementChild);
+itemlist.firstElementChild.textContent="hello 1";
+
+
+
+console.log(itemlist.lastChild);
+console.log(itemlist.lastElementChild);
+itemlist.lastElementChild.textContent="hello 4";
+
+console.log(itemlist.nextSibling);
+console.log(itemlist.nextElementSibling);
+
+
+console.log(itemlist.previousSibling);
+console.log(itemlist.previousElementSibling);
+
+//create element
+// create a div 
+var newDiv=document.createElement('div');
+newDiv.className='Hello';
+newDiv.id="Hello1";
+newDiv.setAttribute('title','Hello Div');
+// create text node 
+
+var newText=document.createTextNode('Hello World');
+newDiv.appendChild(newText);
+var container=document.querySelector('header .container');
+var h1=document.querySelector('header h1');
+container.insertBefore(newDiv,h1);
