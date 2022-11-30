3fa06e2872dd87b4450c3998768714917145c32f
dheeraj303 <singh.dheeraj6303@gmail.com>
2022-11-30 15:21:24 +0530

    student with age object

--- call.js
+++ call.js
@@ -1,7 +1,12 @@
 var obj={num:2};
+var student={age:20};
 var add=function(a,b,c){
     return this.num+a+b+c;
 }
+var printage=function(){
+    console.log(this.age);
+}
+printage.call(student);
 
 console.log(add.call(obj,1,2,3));
 var arr=[1,2,3];
