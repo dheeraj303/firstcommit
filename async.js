7d694046e84563cad8d6cee5a040bd1ffd615ccb
dheeraj303 <singh.dheeraj6303@gmail.com>
2022-12-05 12:49:23 +0530

    async and promise

---
+++ async.js
@@ -0,0 +1,23 @@
+
+
+
+// const premovie=async ()=>{
+//     const promisewifebringingtickets=new Promise((resolve,reject)=>{
+//         setTimeout(()=> resolve('ticket'));
+//     });
+
+//     const getpopcorn=new Promise((resolve,reject)=>resolve('popcorn'));
+//     const butter=new Promise((resolve,reject)=>resolve('butter'));
+    
+
+//     let ticket =await promisewifebringingtickets;
+
+//     console.log(`${ticket}`);
+//     popcorn=await getpopcorn;
+//     console.log(`${popcorn}`);
+//     addbutter= await butter;
+//     console.log(`${addbutter}`);
+//     return ticket;
+// }
+
+// premovie().then((m)=>console.log(m));
--- promise.js
+++ promise.js
@@ -1,52 +1,114 @@
-posts=[{title:"post1", body:"this is post 1"}];
+var posts=[{title:"post1", body:"this is post 1"}];
 
-function getposts(){
-    setTimeout(()=>{
-    let output="";
-    posts.forEach((post)=>{
-        output+=`<li>${post.title}</li>`
-    });
-    document.body.innerHTML=output;
-},1000);
-setTimeout(getposts,1000);
-}
+// function getposts(){
+//     setTimeout(()=>{
+//     let output="";
+//     posts.forEach((post)=>{
+//         output+=`<li>${post.title}</li>`
+//     });
+//     document.body.innerHTML=output;
+// },1000);
+// setTimeout(getposts,1000);
+// }
 
-function createpost(post){
- return new Promise((resolve,reject)=>{
-    setTimeout(()=>{
-        // post.createdAt=Date.now();
-        posts.push(post);
-        const error =false;
-        if(!error){
-            resolve();
-        }else{
-            reject('Error: Something went wrong');
-        }
+// function createpost(post){
+//  return new Promise((resolve,reject)=>{
+//     setTimeout(()=>{
+//         // post.createdAt=Date.now();
+//         posts.push(post);
+//         const error =false;
+//         if(!error){
+//             resolve();
+//         }else{
+//             reject('Error: Something went wrong');
+//         }
       
-    },1000);
-});
-}
+//     },1000);
+// });
+// }
 
-function deletepost(){
-    return new Promise((resolve,reject)=>{
+
+const createpost=async ()=>{
+    const create=(post)=>{ 
+        return new Promise((resolve,reject)=>{
+         setTimeout(()=>{
+           // post.createdAt=Date.now();
+         
+           posts.push(post);
+           console.log(posts);
+           const error =false;
+           if(!error){
+               resolve();
+           }else{
+               reject('Error: Something went wrong');
+           }
+         
+       },1000);
+    }
+    )};
+    
+//    console.log('createpost1');
+
+   const getposts=()=>{return new Promise((resolve, reject) => {
         setTimeout(()=>{
-               
+                let output="";
+                posts.forEach((post)=>{
+                    output+=`<li>${post.title}</li>`
+                });
+                document.body.innerHTML=output;
+            },1000);
+            // setTimeout(getposts,1000);
+            resolve();
+        });
+    }
+    
+    let createpost1=await create({title:'post two',body:'this is post two'});
+    getposts();
+
+ const deletem = new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            //    console.log(posts.length);
                 if(posts.length!=0){
                     posts.pop();
+                    // console.log(posts);
                     resolve();
                 }else{
                     reject('Array is empty');
                 }
         },1000);
     });
+
+    
+await deletem;
+ getposts();
 }
-createpost({title:'post two',body:'this is post two'})
-.then(getposts)
-.catch(err=>console.log(err));
-createpost({title:'post three',body:'this is post three'})
-.then(getposts)
-.catch(err=>console.log(err));
-console.log(posts);
+ 
+//    await getposts;
+  
+   
+
+   createpost().then(()=>console.log('done'));
+
+// function deletepost(){
+//     return new Promise((resolve,reject)=>{
+//         setTimeout(()=>{
+               
+//                 if(posts.length!=0){
+//                     posts.pop();
+//                     resolve();
+//                 }else{
+//                     reject('Array is empty');
+//                 }
+//         },1000);
+//     });
+// }
+// createpost({title:'post two',body:'this is post two'})
+// .then(getposts)
+// .catch(err=>console.log(err));
+// createpost({title:'post three',body:'this is post three'})
+// .then(getposts)
+// .catch(err=>console.log(err));
+// console.log(posts);
 
 // deletepost().then(()=>
 // {getposts();
@@ -63,49 +125,49 @@ console.log(posts);
 // .then(getposts).then(getposts).catch(err=>console.log(err));
 
 
-const promise1=Promise.resolve('HelloWorld');
-const promise2=10;
-const promise3=new Promise((resolve,reject)=>{
-    setTimeout(resolve,2000,'Goodbye')
-});
+// const promise1=Promise.resolve('HelloWorld');
+// const promise2=10;
+// const promise3=new Promise((resolve,reject)=>{
+//     setTimeout(resolve,2000,'Goodbye')
+// });
 
 // Promise.all([promise1,promise2,promise3]).then(values=>console.log(values));
-class User{
-    constructor(){
-        this.lastactivity=Date.now();
-    }
-    getactivity(){
-        console.log(this.lastactivity);
-    }
-}
-var u1=new User();
-function updatelastuseractiviytime(){
-    return new Promise((resolve,reject)=>{
-        u1.lastactivity=Date.now();
-        resolve();
-    })
-}
+// class User{
+//     constructor(){
+//         this.lastactivity=Date.now();
+//     }
+//     getactivity(){
+//         console.log(this.lastactivity);
+//     }
+// }
+// var u1=new User();
+// function updatelastuseractiviytime(){
+//     return new Promise((resolve,reject)=>{
+//         u1.lastactivity=Date.now();
+//         resolve();
+//     })
+// }
 
-Promise.all([createpost({title:'post two',body:'this is post two'})
-,updatelastuseractiviytime()]).then((values)=>{
-    console.log(posts);
-    u1.getactivity();
-});
+// Promise.all([createpost({title:'post two',body:'this is post two'})
+// ,updatelastuseractiviytime()]).then((values)=>{
+//     console.log(posts);
+//     u1.getactivity();
+// });
    
 // console.log(posts);
 // console.log(u1.lastactivity);
 
 
-deletepost().then(()=>
-{getposts();
-    deletepost().then(()=>{
-        getposts();
-        deletepost().then(()=>{
-            getposts();
-            deletepost().then(()=>{
-                getposts();
-            }).catch(err=>console.log(err));
-        }).catch(err=>console.log(err));
-    }).catch(err=>console.log(err));
-}).catch(err=>console.log(err));
+// deletepost().then(()=>
+// {getposts();
+//     deletepost().then(()=>{
+//         getposts();
+//         deletepost().then(()=>{
+//             getposts();
+//             deletepost().then(()=>{
+//                 getposts();
+//             }).catch(err=>console.log(err));
+//         }).catch(err=>console.log(err));
+//     }).catch(err=>console.log(err));
+// }).catch(err=>console.log(err));
 // .then(getposts).then(getposts).catch(err=>console.log(err));
